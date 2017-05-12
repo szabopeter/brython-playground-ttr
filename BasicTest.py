2cc82f32ec1dd7a51b273d318a9527757c4610da
Szabó Péter <szabopeter@github.com>
2017-05-12 08:42:13 +0200

    Skeleton

---
+++ BasicTest.py
@@ -0,0 +1,12 @@
+import unittest
+from main import GameState
+
+
+class BasicTestCase(unittest.TestCase):
+    def testPosEquality(self):
+        pass
+        self.assertEqual("OK", "OK")
+
+
+if __name__ == '__main__':
+    unittest.main()
