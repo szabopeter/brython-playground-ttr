975472b9d42885ade1004b4268bcd2e0d1cbe627
Szabó Péter <1254135+szabopeter@users.noreply.github.com>
2018-03-27 09:35:06 +0200

    Split templateless and templating versions

--- without_template.py
+++ without_template.py
@@ -11,12 +11,6 @@ length_values = {
     7: 25,
     }
 
-remaining_pieces = 45
-train_lengths = [1, 2, 3, 4, 5, 6, 7]
-counts = {length: 0 for length in train_lengths}
-score = [0]
-remaining = [remaining_pieces]
-
 
 def log(msg):
     browser.doc['messages'].text = msg
@@ -26,43 +20,6 @@ def log(msg):
 def log_event(msg, event, element):
     log("%r target=%r data=%r element=%r" % (msg, dir(event.target), event.target.attributes['data-divnr'].value, element, ))
 
-
-def get_divnr(event):
-    return int(event.target.parent.attributes['data-divnr'].value)
-
-
-def update_score():
-    s = 0
-    r = remaining_pieces
-    for l in train_lengths:
-        s += length_values[l] * counts[l]
-        r -= l * counts[l]
-    browser.doc['out_score'].text = score[0] = s
-    browser.doc['out_remaining'].text = remaining[0] = r
-    
-
-def increase(event, element):
-    # log_event("inc", event, element)
-    divnr = get_divnr(event)
-    counts[divnr] += 1
-    browser.doc['count%s' % divnr].text = counts[divnr] 
-    update_score()
-
-
-def decrease(event, element):
-    # log_event("dec", event, element)
-    divnr = get_divnr(event)
-    counts[divnr] -= 1
-    browser.doc['count%s' % divnr].text = counts[divnr] 
-    update_score()
-
-
-Template(browser.doc['input_divs_wrapper'], [increase, decrease]).render(
-    train_lengths=train_lengths)
-Template(browser.doc['out_remaining']).render(remaining=remaining)
-Template(browser.doc['out_score']).render(score=score)
-
-# ------------------------
 
 lengths = {}
 for i in (1, 2, 3, 4, 5, 6):
