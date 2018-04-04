8174a8bfca85b104f070f91e9314d7692b229999
Szabó Péter <1254135+szabopeter@users.noreply.github.com>
2018-04-04 10:50:03 +0200

    Refactoring: extract id building logic and decrease number of browser.doc references

--- playercontrol.py
+++ playercontrol.py
@@ -6,27 +6,36 @@ class PlayerControl:
     def __init__(self, player_number):
         self.nr = player_number
 
+    def get_element(self, prefix, args=None):
+        if args is None:
+            args = (self.nr, )
+        eid = prefix + "_".join([str(arg) for arg in args])
+        return browser.doc[eid]
+
     def update_count(self, length, count):
         if count == 0:
             count = ""
-        browser.doc['count%s_%s' % (self.nr, length, )].text = count
+
+        self.get_element("count", (self.nr, length)).text = count
 
     def update_train_score(self, train_score):
         if train_score == 0:
             train_score = ""
-        browser.doc['out_score%s' % self.nr].text = train_score
+
+        self.get_element("out_score").text = train_score
 
     def update_total_score(self, total_score):
-        browser.doc['total_score%s' % self.nr].text = total_score
+        self.get_element("total_score").text = total_score
 
     def update_remaining(self, remaining):
-        div = browser.doc['out_remaining%s' % self.nr]
+        div = self.get_element("out_remaining")
         if remaining is None or remaining == "":
             div.text = ""
             return
 
         div.text = remaining
 
+        # TODO: these constants belong to GameConfig, the logic to Player
         if remaining > 2:
             div.classList.remove('invalid')
             div.classList.remove('finished')
@@ -38,65 +47,57 @@ class PlayerControl:
             div.classList.add('invalid')
 
     def update_additional_total(self, text):
-        browser.doc['additional_total%s' % self.nr].text = text
-
-    def get_additional_points(self):
-        textbox = browser.doc['additional_points%s' % self.nr]
-        return textbox.value
-
-    def get_name(self):
-        return browser.doc['player_name%s' % self.nr].value
+        self.get_element("additional_total").text = text
 
     def update_name(self, name):
-        browser.doc['player_name%s' % self.nr].value = name
+        self.get_element("player_name").value = name
 
     def set_tickets_entered(self, text):
-        browser.doc['additional_points%s' % self.nr].value = text
+        self.get_element("additional_points").value = text
 
     def mark_additional_points_valid(self):
-        textbox = browser.doc['additional_points%s' % self.nr]
+        textbox = self.get_element("additional_points")
         textbox.classList.remove('invalid')
         textbox.classList.add('valid')
 
     def mark_additional_points_invalid(self):
-        textbox = browser.doc['additional_points%s' % self.nr]
+        textbox = self.get_element("additional_points")
         textbox.classList.remove('valid')
         textbox.classList.add('invalid')
         self.update_additional_total("?!")
 
     def mark_additional_points_neutral(self):
-        textbox = browser.doc['additional_points%s' % self.nr]
+        textbox = self.get_element("additional_points")
         textbox.classList.remove('valid')
         textbox.classList.remove('invalid')
 
     def set_longest_road(self, value):
-        textbox = browser.doc['longest_road_length%s' % self.nr]
-        classlist = textbox.classList
+        classlist = self.get_element("longest_road_length").classList
         if value:
             classlist.add('has_longest_road')
         else:
             classlist.remove('has_longest_road')
 
     def set_longest_road_length(self, value):
-        textbox = browser.doc['longest_road_length%s' % self.nr]
         if value == 0:
             value = ""
-        textbox.value = value
+
+        self.get_element("longest_road_length").value = value
 
     def mark_longest_road_length_invalid(self):
-        textbox = browser.doc['longest_road_length%s' % self.nr]
+        textbox = self.get_element("longest_road_length")
         classlist = textbox.classList
         classlist.add('invalid')
         classlist.remove('valid')
 
     def mark_longest_road_length_valid(self):
-        textbox = browser.doc['longest_road_length%s' % self.nr]
+        textbox = self.get_element("longest_road_length")
         classlist = textbox.classList
         classlist.add('valid')
         classlist.remove('invalid')
 
     def mark_longest_road_length_neutral(self):
-        textbox = browser.doc['longest_road_length%s' % self.nr]
+        textbox = self.get_element("longest_road_length")
         classlist = textbox.classList
         classlist.remove('valid')
         classlist.remove('invalid')
@@ -110,7 +111,7 @@ class PlayerControl:
         show_div("player_view_normal%s" % self.nr)
 
     def update_color(self, color):
-        div = browser.doc['player_section%s' % self.nr]
+        div = self.get_element("player_section")
         class_list = div.classList
         new_color_class_name = "player_color_%s" % color
         class_list.add(new_color_class_name)
