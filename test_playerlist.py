8c46bb2f5d169b8e238bac2b8080e354406326da
Szabó Péter <1254135+szabopeter@users.noreply.github.com>
2018-04-10 10:37:31 +0200

    Playerlist restore_and_move_up

--- test_playerlist.py
+++ test_playerlist.py
@@ -8,9 +8,16 @@ class PlayerMock:
         self.nr = nr
         self.control = PlayerControlMock(nr) if control is None else control
         self.has_refreshed = False
+        self.is_minimized = False
 
     def update_all(self):
         self.has_refreshed = True
+
+    def minimize(self):
+        self.is_minimized = True
+
+    def restore(self):
+        self.is_minimized = False
 
 
 class PlayerControlMock:
@@ -32,15 +39,36 @@ class PlayerList:
     def get_players(self):
         return self.players
 
-    def move_to_last(self, player):
+    def minimize_and_move_to_last(self, player_moving):
         controls = [p.control for p in self.players]
-        self.players.remove(player)
-        self.players.append(player)
+        self.players.remove(player_moving)
+        self.players.append(player_moving)
         for i in range(len(controls)):
             player = self.players[i]
             if player.control != controls[i]:
                 player.control = controls[i]
                 player.update_all()
+
+        player_moving.minimize()
+
+    def restore_and_move_up(self, player_moving):
+        controls = [p.control for p in self.players]
+        self.players.remove(player_moving)
+        for i in range(len(self.players)):
+            if self.players[i].is_minimized:
+                self.players.insert(i, player_moving)
+                break
+
+        if player_moving not in self.players:
+            self.players.append(player_moving)
+
+        for i in range(len(controls)):
+            player = self.players[i]
+            if player.control != controls[i]:
+                player.control = controls[i]
+                player.update_all()
+
+        player_moving.restore()
 
 
 class PlayerListTestCase(unittest.TestCase):
@@ -55,9 +83,36 @@ class PlayerListTestCase(unittest.TestCase):
         actual_refresh = [player.has_refreshed for player in playerlist.get_players()]
         self.assertSequenceEqual(actual_refresh, expected_refresh)
 
+    def assert_is_minimized(self, playerlist, expected_is_minimized):
+        actual_is_minimized = [player.is_minimized for player in playerlist.get_players()]
+        self.assertSequenceEqual(actual_is_minimized, expected_is_minimized)
+
     def test_minimize_moves_to_last(self):
         players = [PlayerMock(nr, default_controls[nr]) for nr in range(player_count)]
         pl = PlayerList(players)
-        pl.move_to_last(players[2])
+        pl.minimize_and_move_to_last(players[2])
         self.assert_order(pl, (0, 1, 3, 4, 2))
         self.assert_has_refreshed(pl, (False, False, True, True, True))
+        self.assert_is_minimized(pl, (False, False, False, False, True))
+
+    def test_restore_moves_up(self):
+        players = [PlayerMock(nr, default_controls[nr]) for nr in range(player_count)]
+        for player in players:
+            player.is_minimized = player.nr >= 2
+
+        pl = PlayerList(players)
+        self.assert_is_minimized(pl, (False, False, True, True, True))
+        pl.restore_and_move_up(players[-1])
+        self.assert_order(pl, (0, 1, 4, 2, 3))
+        self.assert_has_refreshed(pl, (False, False, True, True, True))
+        self.assert_is_minimized(pl, (False, False, False, True, True))
+
+    def test_restore_single_stays_in_place(self):
+        players = [PlayerMock(nr, default_controls[nr]) for nr in range(player_count)]
+        players[-1].is_minimized = True
+        pl = PlayerList(players)
+        self.assert_is_minimized(pl, (False, False, False, False, True))
+        pl.restore_and_move_up(players[-1])
+        self.assert_order(pl, (0, 1, 2, 3, 4))
+        self.assert_has_refreshed(pl, (False, False, False, False, False))
+        self.assert_is_minimized(pl, (False, False, False, False, False))
